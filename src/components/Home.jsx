96dd0494280132bdbf33b43bcbb99d8542f1eda9
Ojas <ojastulswani@gmail.com>
2023-06-05 19:25:12 +0530

    Initiall commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+
+
+import Code from "./Code";
+import Hearder from "./Header";
+import Result from "./Result";
+
+const Home = () => {
+
+    return (
+        <>
+        <Hearder />
+        <Code />
+        <Result />
+        </>
+    )
+}
+
+export default Home;
